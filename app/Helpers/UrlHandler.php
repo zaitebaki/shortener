f8a77725a1d5fe26b168147ef56e83df5cea6b2f
ft17 <valery@churbanov74.ru>
2020-04-20 12:56:33 +0400

    Apply hashids for getting tokens

--- app/Helpers/UrlHandler.php
+++ app/Helpers/UrlHandler.php
@@ -14,40 +14,14 @@ class UrlHandler
         )) {
             return true;
         }
-
         return false;
     }
 
-    /**
-     * Получить токен для URL.
-     *  @param string $url
-     *  @return string
-     */
-    public static function getShortToken(string $url): string
+    public static function getNewToken(): string
     {
-        $currentToken = config('data.currentToken');
+        $milliseconds = (int) round(microtime(true) * 1000);
+        $hashids = new Hashids($milliseconds, 5);
 
-        $smbArrays = require_once 'token-array.php';
-
-        $countZ = 0;
-        for ($i = 4; $i > 0; $i--) {
-            if ($currentToken === 'Z') {
-                $countZ++;
-            } else {
-                break;
-            }
-        }
-
-        for ($i = 0, $j = 4; $i < $countZ; $i++, $j--) {
-            $currentToken[$j] = $smbArrays['smbOrder'][0];
-        }
-
-        $incrementSmb = $currentToken[4 - $countZ];
-        $newOrderNumber = $smbArrays['smbValuesArray'][$incrementSmb];
-        $currentToken[4 - $countZ] = $smbArrays['smbOrder'][$newOrderNumber];
-
-        config(['data.currentToken' => $currentToken]);
-
-        return $currentToken;
+        return $hashids->encode(1);
     }
 }
