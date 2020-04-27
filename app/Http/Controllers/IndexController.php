af9b68f016a96bc42aa81d3a0ec7ac1ad260b0e1
ft17 <valery@churbanov74.ru>
2020-04-27 09:17:46 +0400

    Check length input url

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -70,7 +70,7 @@ class IndexController extends SuperController
         $statisticLink = '';
 
         // проверить url на валидность
-        if (is_string($userLink) && UrlHandler::isValid($userLink)) {
+        if (is_string($userLink) && strlen($userLink) < 2048 && UrlHandler::isValid($userLink)) {
             $token = UrlHandler::getNewToken();
             UrlHandler::saveUrl($userLink, $token, $date);
             $shortLink = env('APP_URL') . $token;
